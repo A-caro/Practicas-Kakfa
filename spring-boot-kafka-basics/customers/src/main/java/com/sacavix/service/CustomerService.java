95f64dc9ac27e1e2bfae325cf7f8ebca4751cc39
Carolina Atampis <carolinanatampis@gmail.com>
2022-07-14 18:13:49 -0300

    "Actualizacion"

--- spring-boot-kafka-basics/customers/src/main/java/com/sacavix/service/CustomerService.java
+++ spring-boot-kafka-basics/customers/src/main/java/com/sacavix/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.sacavix.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.sacavix.model.Customer;
--- spring-boot-kafka-basics/notifications/src/main/java/com/sacavix/service/CustomerService.java
+++ spring-boot-kafka-basics/notifications/src/main/java/com/sacavix/service/CustomerService.java
@@ -4,13 +4,18 @@ package com.sacavix.service;
 import com.sacavix.model.Customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
 @Slf4j
+@Component
 public class CustomerService {
+
     @KafkaListener(topics = "${topic.customer.name:customers}",
             containerFactory = "kafkaListenerContainerFactory", groupId = "grupo1") //va a recibir el mensaje del productor
+
     public void consumer(Customer customer){
         if (customer.getClass().isAssignableFrom(Customer.class)){
-            log.info("El objeto recibido es {}", customer);
+            log.info("El objeto recibido es {} ", customer);
         }
 
     }
